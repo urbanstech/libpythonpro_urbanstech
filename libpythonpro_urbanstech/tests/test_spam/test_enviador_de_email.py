51c9ad63543dcc2352d402a2215743120c88b86e
urbanstech <marcos.urbanski@gmail.com>
2025-05-24 16:46:55 -0300

    TDD e baby Steps

--- libpythonpro_urbanstech/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro_urbanstech/tests/test_spam/test_enviador_de_email.py
@@ -1,6 +1,16 @@
-from libpythonpro_urbanstech.spam.test_enviador_de_email import Enviador
+from libpythonpro_urbanstech.spam.enviador_de_email import Enviador
 
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
+
+def test_remetente():
+    enviador = Enviador()
+    resultado  = enviador.enviar(
+        'marcos.urbanski@gmail.com',
+        'marcos_urbanski@hotmail.com',
+        'Cursos Python Pro',
+        'Primeira turma Guido Von Rossum aberta.'
+    )
+    assert 'marcos.urbanski@gmail.com' in resultado
