36a0d5f45c2dc952dbc52b8b31cec75286032dd7
urbanstech <marcos.urbanski@gmail.com>
2025-05-25 10:04:34 -0300

    ntegrado a feature parametrize do pytest

--- libpythonpro_urbanstech/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro_urbanstech/tests/test_spam/test_enviador_de_email.py
@@ -1,17 +1,21 @@
 from libpythonpro_urbanstech.spam.enviador_de_email import Enviador
-
+import pytest
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
 
-def test_remetente():
+@pytest.mark.parametrize(
+    'destinatario',
+    ['foo@bar.com.br', 'marcos.urbanski@gmail.com']
+)
+def test_remetente(destinatario):
     enviador = Enviador()
     resultado = enviador.enviar(
-        'marcos.urbanski@gmail.com',
+        destinatario,
         'marcos_urbanski@hotmail.com',
         'Cursos Python Pro',
         'Primeira turma Guido Von Rossum aberta.'
     )
-    assert 'marcos.urbanski@gmail.com' in resultado
+    assert destinatario in resultado
