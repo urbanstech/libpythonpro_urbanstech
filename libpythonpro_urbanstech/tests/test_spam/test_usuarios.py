66006113424c0f56f94109a9897591c09528681f
urbanstech <marcos.urbanski@gmail.com>
2025-05-26 10:30:12 -0300

    alterado nome do usuario

--- libpythonpro_urbanstech/tests/test_spam/test_usuarios.py
+++ libpythonpro_urbanstech/tests/test_spam/test_usuarios.py
@@ -5,7 +5,7 @@ from libpythonpro_urbanstech.spam.modelos import Usuario
 def test_salvar_usuario():
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
-    usuario = Usuario(nome='Renzo')
+    usuario = Usuario(nome='marcos')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
     sessao.roll_back()
@@ -22,3 +22,4 @@ def test_listar_usuarios():
     assert usuarios == sessao.listar()
     sessao.roll_back()
     sessao.fechar()
+    conexao.fechar()
