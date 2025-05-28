973ad1ab765f0fc4a2c550e1116c2786b830a059
urbanstech <marcos.urbanski@gmail.com>
2025-05-28 14:10:39 -0300

    Isolamento de imports

--- libpythonpro_urbanstech/tests/test_spam/test_github_api.py
+++ libpythonpro_urbanstech/tests/test_spam/test_github_api.py
@@ -1,13 +1,27 @@
 from unittest.mock import Mock
 from libpythonpro_urbanstech import github_api
+import pytest
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/35203750?v=4'
     resp_mock.json.return_value = {
         "login": "urbanstech", "id": 35203750,
-        "avatar_url": "https://avatars.githubusercontent.com/u/35203750?v=4"
+        "avatar_url": url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('urbanstech')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('urbanstech')
     assert 'https://avatars.githubusercontent.com/u/35203750?v=4' == url
