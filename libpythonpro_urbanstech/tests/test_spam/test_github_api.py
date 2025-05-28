7dc6e0a8c60499a250e97753949b55583158463f
urbanstech <marcos.urbanski@gmail.com>
2025-05-28 10:17:33 -0300

    Módulo como Objeto fixed pep8 issues

--- libpythonpro_urbanstech/tests/test_spam/test_github_api.py
+++ libpythonpro_urbanstech/tests/test_spam/test_github_api.py
@@ -5,8 +5,8 @@ from libpythonpro_urbanstech import github_api
 def test_buscar_avatar():
     resp_mock = Mock()
     resp_mock.json.return_value = {
-        "login":"urbanstech","id":35203750,
-        "avatar_url":"https://avatars.githubusercontent.com/u/35203750?v=4"
+        "login": "urbanstech", "id": 35203750,
+        "avatar_url": "https://avatars.githubusercontent.com/u/35203750?v=4"
     }
     github_api.requests.get = Mock(return_value=resp_mock)
     url = github_api.buscar_avatar('urbanstech')
